80595c3bbc5817aca49f98d4e559aa5fd566e042
Danilo-MartinsM <danilo.martins.dev@gmail.com>
2025-03-03 09:27:51 -0300

    Create jogoNumeros.py

---
+++ jogoNumeros.py
@@ -0,0 +1,22 @@
+import random 
+numero_aleatorio = random.randrange(0, 100)
+minimo = 0
+maximo = 100
+perda = 0
+while True:
+    chute = int(input(f"Digite um número entre {minimo} e {maximo}: "))
+    if chute < minimo or chute > maximo:
+        print("Fora do alcance")
+        continue
+        
+    if chute > numero_aleatorio:
+        maximo = chute
+    elif chute < numero_aleatorio:
+        minimo = chute
+    else:
+        print("Correto!")
+        break
+    perda = maximo - minimo
+    if perda == 2:
+        print("Perdeu playboy")
+        break
